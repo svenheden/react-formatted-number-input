e8d4cee96c3db35ddb7f96a2279ffff15fd4813a
Jonathan Svenheden <jonathan@svenheden.com>
2019-09-04 14:09:17 +0200

    Update dependencies

--- src/index.test.tsx
+++ src/index.test.tsx
@@ -1,5 +1,5 @@
 import * as React from 'react';
-import { render, fireEvent, cleanup } from 'react-testing-library';
+import { render, fireEvent, cleanup } from '@testing-library/react';
 import { createFormattedNumberInput } from './';
 
 const Input: React.StatelessComponent<any> = ({ innerRef, ...rest }) => <input ref={innerRef} {...rest} />;
